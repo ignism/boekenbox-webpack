62b50688225f67f8dee16ca007b0902ce70698b5
ignism <mark.brand@gmail.com>
2017-07-04 14:11:17 +0200

    mobile ready

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,54 @@
-document.write('<p>hello world</p>')
+$(document).ready(function() {
+  resizeThemeBlocks()
+
+  $("#slick-header").slick({
+    "arrows":false,
+    //"draggable":false,
+    "fade":true,
+    "infinite":false
+  });
+
+  $("#slick-form").slick({
+    "arrows":false,
+    //"draggable":false,
+    "fade":true,
+    "infinite":false
+  });
+
+  $(".slick-goto").click(function() {
+    var number = $(this).data('slide-number')
+    $("#slick-header").slick('slickGoTo', number);
+    $("#slick-form").slick('slickGoTo', number);
+  })
+
+  $("#slick-next").click(function() {
+    $("#slick-header").slick('slickNext');
+    $("#slick-form").slick('slickNext');
+  })
+
+  $("#slick-prev").click(function() {
+    $("#slick-header").slick('slickPrev');
+    $("#slick-form").slick('slickPrev');
+  })
+});
+
+$(window).resize(function () {
+  resizeThemeBlocks()
+})
+
+function resizeThemeBlocks() {
+  var blockH = 120;
+  var h = 0;
+
+  $(".theme-text").each(function () {
+    var th = $(this).height();
+
+    if (th > h) {
+      h = th;
+    }
+  })
+
+  $(".theme-selection--block").each(function () {
+    $(this).height(blockH + h);
+  })
+}
